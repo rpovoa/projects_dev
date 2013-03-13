599021fc568c6658f46d002cd97cf725021b9fb6
Rodolfo Póvoa Leal <rodpovoa@gmail.com>
2013-03-13 14:33:38 -0300

    Exercicio em C

---
+++ EXE_01.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	
+    n1,n2,n3,mediaa float;
+    cod_aluno int;
+     printf("\n Digite o codigo do aluno");
+     scanf("%d",cod_aluno);
+     printf("\n Digite a nova 1");
+      scanf("%f", n1);
+     printf("\n Digite a nova 2");
+      scanf("%f", n2);
+     printf("\n Digite a nova 3");
+      scanf("%f", n3);
+
+      if (mediaa = 0)
+      
+      end
+
+      else
+
+      	 mediaa = (n1+n2+n3)/3;
+
+      	printf("%f\n A media do auno é", mediaa );
+
+        system("pause");
+	return 0;
+}
